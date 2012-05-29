37850d2c14297d18b3d15cbd9ec88dee3b5e247e
robbyloveyou@gmail.com <robbyloveyou@gmail.com@20d7f8cc-deda-21e5-22c6-f59a9af16849>
2012-05-29 14:24:00 +0000

    基本框架搭建

---
+++ trunk/KoalaGIS.Web.OLMap/KoalaGIS.Web.OLMap/KoalaGIS.Web.OLMap/KoalaGIS/container/OLMapView.js
@@ -0,0 +1,37 @@
+﻿//
+//author:zhj
+//date:2012-05-29 12:30
+//在线地图app
+
+Ext.define('KoalaGIS.container.OLMapView', {
+    extend: 'Ext.container.Viewport',
+    layout: 'border',
+    items: [{
+        region: 'north',
+        html: 'Logo and Title',
+        border: false,
+        margins: '0 0 5 0'
+    }, {
+        region: 'west',
+        collapsible: true,
+        title: 'Navigation',
+        width: 150
+    }, {
+        region: 'south',
+        title: 'South Panel',
+        collapsible: true,
+        html: 'Information goes here',
+        split: true,
+        height: 100,
+        minHeight: 100
+    }, {
+        region: 'center',
+        xtype: 'tabpanel', // TabPanel itself has no title
+        activeTab: 0,      // First tab active by default
+        items: {
+            title: 'Default Tab',
+            html: '<div id="divMap" style="width:100%;height:100%; border:solid 1px red">The first tab\'s content. Others may be added dynamically</div>'
+        }
+    }]
+
+});
