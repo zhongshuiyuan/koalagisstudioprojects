9309a65a9a1b62794a628cd3f893e661c8e8a927
robbyloveyou <robbyloveyou@gmail.com>
2012-05-29 14:24:00 +0000

    基本框架搭建

---
+++ trunk/KoalaGIS.Web.OLMap/KoalaGIS.Web.OLMap/KoalaGIS.Web.OLMap/KoalaGIS/map/Map.js
@@ -0,0 +1,16 @@
+﻿Ext.define('KoalaGIS.map.OpenLayers', {
+    init: function() {
+        var map = new OpenLayers.Map('map', { maxResolution: 0.703125 });
+        var wmscURL = [
+				    "http://wmsc1.terrapages.net/getmap?",
+				    "http://wmsc2.terrapages.net/getmap?",
+				    "http://wmsc3.terrapages.net/getmap?",
+				    "http://wmsc4.terrapages.net/getmap?"
+			    ];
+        var terrapagesStreetLayer = new OpenLayers.Layer.WMS('TerraPages Street', wmscURL, { layers: 'UnprojectedStreet', format: 'image/jpeg' }, { buffer: 1, isBaseLayer: true });
+        map.addLayer(terrapagesStreetLayer);
+        map.zoomToMaxExtent();
+    },
+    addLayer: function() {
+    }
+});
