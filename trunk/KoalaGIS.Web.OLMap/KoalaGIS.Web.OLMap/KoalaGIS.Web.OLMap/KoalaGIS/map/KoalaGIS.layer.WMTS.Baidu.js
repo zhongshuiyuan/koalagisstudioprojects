d067f57a3e3ea14876172b8e4d2092e427c597ce
robbyloveyou@gmail.com <robbyloveyou@gmail.com@20d7f8cc-deda-21e5-22c6-f59a9af16849>
2012-06-01 15:55:29 +0000

    百度地图功能添加

---
+++ trunk/KoalaGIS.Web.OLMap/KoalaGIS.Web.OLMap/KoalaGIS.Web.OLMap/KoalaGIS/map/KoalaGIS.layer.WMTS.Baidu.js
@@ -0,0 +1,28 @@
+﻿
+
+KoalaGIS.Layer.WMTS.Baidu = OpenLayers.Class(OpenLayers.Layer.WMTS, {
+
+    version: '1.0.0',
+    url: 'http://www.baidu.com',
+    layer: 'KOALAGIS_LAYER_WMTS_BAIDU',
+    tileOrigin: null,
+    tileFullExtent: null,
+
+    initialize: function(config) {
+
+    },
+
+    getURL: function(bounds) {
+        bounds = this.adjustBounds(bounds);
+        var url = "";
+        if (!this.tileFullExtent || this.tileFullExtent.intersectsBounds(bounds)) {
+            var center = bounds.getCenterLonLat();
+            var info = this.getTileInfo(center);
+            var matrixId = this.matrix.identifier;
+        }
+
+    },
+
+    CLASS_NAME: 'KoalaGIS.Layer.WMTS.Baidu'
+
+});
