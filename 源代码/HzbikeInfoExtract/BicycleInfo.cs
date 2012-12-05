6607526d31b99832208ac30d9ff2df6576d0b7aa
robbyloveyou@gmail.com <robbyloveyou@gmail.com@20d7f8cc-deda-21e5-22c6-f59a9af16849>
2012-12-05 09:14:25 +0000

    从hzbus抓取自行车数据程序

---
+++ 源代码/HzbikeInfoExtract/BicycleInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KoalaGIS.EasyHz.Model
+{
+    /// <summary>
+    ///BicycleInfo 的摘要说明
+    /// </summary>
+    public class BicycleInfo
+    {
+        public BicycleInfo()
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+        }
+        /// <summary>
+        /// 站点名称
+        /// </summary>
+        public string StationName { get; set; }
+
+        /// <summary>
+        /// 站点编号
+        /// </summary>
+        public string StationID { get; set; }
+
+        /// <summary>
+        /// 可借车辆
+        /// </summary>
+        public int BorrowNum { get; set; }
+
+        /// <summary>
+        /// 可还车辆
+        /// </summary>
+        public int ReturnNum { get; set; }
+
+        /// <summary>
+        /// 服务时间
+        /// </summary>
+        public string ServiceTime { get; set; }
+
+        /// <summary>
+        /// 值守状态
+        /// </summary>
+        public string ServiceState { get; set; }
+
+        /// <summary>
+        /// 站点地址
+        /// </summary>
+        public string StationAddr { get; set; }
+
+        /// <summary>
+        /// 站点X坐标
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// 站点X坐标
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// 服务电话
+        /// </summary>
+        public string ServicePhone { get; set; }
+
+        /// <summary>
+        /// 站点备注
+        /// </summary>
+        public string StationRemarks { get; set; }
+
+        /// <summary>
+        /// 将自行车信息以json方式输出
+        /// </summary>
+        /// <returns></returns>
+        public string ToJson()
+        {
+            return string.Format("{{StationName:'{0}', BorrowNum:'{1}',ReturnNum: '{2}', StationAddr:'{3}', X:{4}, Y:{5}}}", this.StationName, this.BorrowNum, this.ReturnNum,this.StationAddr, this.X, this.Y);
+        }
+
+    }
+}
---
+++ 源代码/HzbikeInfoExtract/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KoalaGIS.EasyHz.BikeInfoExtractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //KoalaGIS.EasyHz.Utils.BikeExtract test = new Utils.BikeExtract();
+            //test.Test("http://www.hzbus.cn/Page/NearbyBicyle.aspx?w=500&x=120.19879038464029&y=30.28017029916467");
+
+            KoalaGIS.EasyHz.Utils.BikeExtract grasp = new Utils.BikeExtract();
+            grasp.PageInfoEvent += new Utils.PageInfoHandler(grasp_PageInfoEvent);
+            grasp.ExportData("D:");
+
+            MessageBox.Show("哇嘎嘎！抓取完成了！");
+        }
+
+        void grasp_PageInfoEvent(int total, int cur)
+        {
+            this.progressBar1.Value = cur;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.progressBar1.Maximum = 232;
+            this.progressBar1.Minimum = 0;
+            
+            //KoalaGIS.EasyHz.Utils.AspNetPageGrap grap = new Utils.AspNetPageGrap();
+            //grap.LoadViewStateInfo("http://www.hzbus.cn/Page/BicyleNearby.aspx?w=50&rnd=2");
+            //string result = grap.GetPageInfo("http://www.hzbus.cn/Page/BicyleNearby.aspx?w=50&rnd=2", 3, 4);
+        }
+    }
+}
