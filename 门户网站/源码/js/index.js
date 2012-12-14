507b167770af56fe375b53285031898f77dbd15f
robbyloveyou@gmail.com <robbyloveyou@gmail.com@20d7f8cc-deda-21e5-22c6-f59a9af16849>
2012-12-14 12:06:39 +0000

    添加首页新闻管理模块 添加Header.htm,footer.htm 添加Demos.htm

---
+++ 门户网站/源码/js/index.js
@@ -0,0 +1,46 @@
+﻿//2012-12-14
+//by:zhj
+
+//微软时间格式转换函数
+function MSTimeFormat (input, strict) {
+    // note: the timezone offset is ignored since the MS Ajax server sends
+    // a UTC milliseconds-since-Unix-epoch value (negative values are allowed)
+    var re = new RegExp('\\/Date\\(([-+])?(\\d+)(?:[+-]\\d{4})?\\)\\/');
+    var r = (input || '').match(re);
+    //return r ? new Date(((r[1] || '') + r[2]) * 1) : null;
+    return r ? new Date( r[2] * 1) : null;
+}
+
+function loadNews() {
+    var url = 'News/NewsHandler.ashx';
+    jQuery.ajax(url, {
+        dataType: 'json',
+        data: {
+            request: 'querytopnews',
+            top: 5
+        },
+        error: function () {
+        },
+        success: function (data, textStatus, jqXHR) {
+            var news = data.ResultSet;
+            var html = [];
+            for (var i = 0; i < news.length; i++) {
+                var ID = news[i].ID;
+                var TITLE = news[i].TITLE;
+                var DATE = news[i].POSTTIME;
+                DATE = MSTimeFormat(DATE);
+                DATE = DATE.getFullYear() + '-' + DATE.getMonth() + '-' + DATE.getDay();
+                html.push("<li><a href='' title='" + TITLE + "' target='_blank'>" + TITLE + "</a><span class='data'>" + DATE + "</span></li> ");
+            }
+
+            html = html.join('');
+
+            $('#ulHotNews').html(html);
+        }
+    });
+}
+
+$(document).ready(function () {
+    loadNews();
+});
+
